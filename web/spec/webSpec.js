0b0c98685eb72488476bc0ee58f757a2ae3e10b4
mparker <mparker@pivotal.io>
2017-11-08 16:19:20 +0000

    broken frontend test

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,34 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const React = require("react")
+const ReactDOM = require("react-dom")
+
+class PlayForm extends React.Component {
+    render(){
+        return <button>Hello Test</button>
+    }
+}
+
+
+describe("Play Form", function () {
+    describe("when the request is processed as 'invalid'", function () {
+        it("should display INVALID", function () {
+            let domFixture = document.createElement("div")
+            document.body.appendChild(domFixture)
+
+            let alwaysInvalidRequest = {
+                play: function(p1, p2, observer){
+                    observer.invalid()
+                }
+            }
+
+            ReactDOM.render(
+                <PlayForm requests={alwaysInvalidRequest}/>,
+                domFixture
+            )
+
+            document.querySelector("button").click()
+            expect(domFixture.innerText).toContain("INVALID")
+        })
+
+
     })
 })
