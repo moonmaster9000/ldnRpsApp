98175d6ea0dc323acff9c8254179d27285e85d59
mparker <mparker@pivotal.io>
2017-11-09 14:48:46 +0000

    round repo contract penciled in

--- rps/spec/historySpec.js
+++ rps/spec/historySpec.js
@@ -1,4 +1,5 @@
-const {Requests} = require("../src/rps")
+const {Requests, Round} = require("../src/rps")
+
 
 describe("history", function () {
     describe("when no rounds have been played", function () {
@@ -9,6 +10,66 @@ describe("history", function () {
 
             expect(observer.noRounds).toHaveBeenCalled()
         })
+    })
+
+    describe("when rounds have been played", function () {
+        it("then it sends the rounds to the observer", function () {
+            let requests = new Requests()
+            let playRoundObserver = jasmine.createSpyObj("playRoundObserver", ["invalid"])
+            let repo = {
+                isEmpty(){},
+                findAll(){},
+                save(){}
+            }
+
+            let observer = jasmine.createSpyObj("observer", ["rounds"])
+
+            requests.playRound("foo", "bar", playRoundObserver, repo)
+
+            requests.getHistory(observer, repo)
+
+            expect(observer.rounds).toHaveBeenCalledWith([
+                new Round("foo", "bar", "invalid")
+            ])
+        })
 
     })
 })
+
+function FakeRoundRepo(){
+
+}
+
+fdescribe("round repo", function () {
+    describe("when no rounds have been saved", function () {
+        it("is empty", function () {
+            expect(new FakeRoundRepo().isEmpty()).toBe(true)
+        })
+    })
+
+    describe("when rounds have been saved", function () {
+        it("is not empty", function () {
+
+        })
+
+        it("returns the rounds that have been saved", function () {
+
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- rps/src/rps.js
+++ rps/src/rps.js
@@ -1,14 +1,23 @@
 function Requests() {
-    this.playRound = function (p1Throw, p2Throw, observer) {
-        new PlayRoundRequest(p1Throw, p2Throw, observer).process()
+    this.playRound = function (p1Throw, p2Throw, observer, repo) {
+        new PlayRoundRequest(p1Throw, p2Throw, observer, repo).process()
+    }
+
+    this.getHistory = function(observer, repo){
+        if (repo.isEmpty()){
+            observer.noRounds()
+        } else {
+            observer.rounds(repo.findAll())
+        }
     }
 }
 
-function PlayRoundRequest(p1Throw, p2Throw, observer) {
+function PlayRoundRequest(p1Throw, p2Throw, observer, repo) {
     this.process = function () {
-        if (invalidThrow(p1Throw) || invalidThrow(p2Throw))
+        if (invalidThrow(p1Throw) || invalidThrow(p2Throw)) {
+            repo.save(new Round(p1Throw, p2Throw, "invalid"))
             observer.invalid()
-        else if (tie())
+        } else if (tie())
             observer.tie()
         else if (p1Wins())
             observer.p1Wins()
@@ -35,6 +44,14 @@ function PlayRoundRequest(p1Throw, p2Throw, observer) {
     }
 }
 
+function Round(p1Throw, p2Throw, result){
+    this.p1Throw = p1Throw
+    this.p2Throw = p2Throw
+    this.result = result
+}
+
+
 module.exports = {
-    Requests
+    Requests,
+    Round
 }
